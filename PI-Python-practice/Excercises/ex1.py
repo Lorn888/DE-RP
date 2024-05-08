3aba2329b152fb7a0430a9adbe11bd60b01df72d
Patryk <patrykokpara@hotmail.com>
2024-05-08 22:30:37 +0100

    ex1

--- PI-Python-practice/Excercises/ex1.py
+++ PI-Python-practice/Excercises/ex1.py
@@ -2,3 +2,11 @@
 # a. Create variables for an integer, a float, and a string. Print each variable and its data type.
 # b. Create a list with three different data types (e.g., integer, string, float). Print the list and its length.
 # c. Create a tuple with two elements. Print the tuple and its type.
+
+integer = 9
+float = 0.1
+string = "Testing"
+
+print(integer)
+print(float)
+print(string)
