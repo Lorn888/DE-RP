04ab4638b41b024cfd085638160bec6f6e9243ac
Patryk <patrykokpara@hotmail.com>
2024-05-08 22:25:57 +0100

    Data Types

---
+++ PI-Python-practice/Excercises/ex1.py
@@ -0,0 +1,4 @@
+# Data Types:
+# a. Create variables for an integer, a float, and a string. Print each variable and its data type.
+# b. Create a list with three different data types (e.g., integer, string, float). Print the list and its length.
+# c. Create a tuple with two elements. Print the tuple and its type.
