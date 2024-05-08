f05011111ec70007097e3abd463ed189db43c7a5
Patryk <patrykokpara@hotmail.com>
2024-05-08 22:55:39 +0100

    Control Structures

---
+++ PI-Python-practice/Excercises/ex2.py
@@ -0,0 +1,4 @@
+# Control Structures:
+# a. Write a for loop to print numbers from 1 to 5.
+# b. Write a while loop to print even numbers from 2 to 6.
+# c. Write an if statement to check if a number is greater than 10. Print a message accordingly.
