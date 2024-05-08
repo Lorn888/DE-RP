8bb5bcb20e6042b87df5c0541183d8fadd9d5e66
Patryk <patrykokpara@hotmail.com>
2024-05-08 22:46:07 +0100

    ex2

--- PI-Python-practice/Excercises/ex1.py
+++ PI-Python-practice/Excercises/ex1.py
@@ -3,6 +3,8 @@
 # b. Create a list with three different data types (e.g., integer, string, float). Print the list and its length.
 # c. Create a tuple with two elements. Print the tuple and its type.
 
+# a)
+
 integer = 9
 float = 0.1
 string = "Testing"
@@ -10,3 +12,9 @@ string = "Testing"
 print(integer)
 print(float)
 print(string)
+
+# b)
+
+list = [8,"some text", 0.3]
+
+print (f"this is the list:{list} and it contains {len(list)} items")
