c23b27c174269e0964dc82f16f65de6c68237426
Patryk <patrykokpara@hotmail.com>
2024-05-09 20:37:18 +0100

    ex3

--- PI-Python-practice/Excercises/ex5.py
+++ PI-Python-practice/Excercises/ex5.py
@@ -2,10 +2,19 @@
 #    b. Print the value associated with the "age" key.
 #    c. Modify the value associated with the "city" key and print the updated dictionary.
 
+# a)
 dictionary = {
     "name": "Patryk",
     "age": 32,
     "city": "Wodzislaw-Slaski"
 }
 
+# b)
+
 print(dictionary["age"])
+
+# c)
+
+dictionary["city"] = "London"
+
+print(dictionary["city"])
