609242a8e30043a4acdcb651ce3fec41523f2b09
Patryk <patrykokpara@hotmail.com>
2024-05-09 12:27:42 +0100

    Strings

---
+++ PI-Python-practice/Excercises/ex3.py
@@ -0,0 +1,4 @@
+# a. Create a string variable and print its length.
+# b. Use string concatenation to combine two strings and print the result.
+# c. Use string slicing to print the first three characters of a string.
+
--- PI-Python-practice/practice1.py
+++ PI-Python-practice/practice1.py
@@ -1,1 +1,22 @@
-name= "noche"
+number_apples = 6
+number_apples += 4
+
+print(number_apples)
+
+a = 1
+b = 2
+c = 3
+
+a = c
+c = b
+b = a
+a = c
+
+print(a)
+print(b)
+print(c)
+
+a = "Hello,"
+a+= ' world'
+
+print(a)
