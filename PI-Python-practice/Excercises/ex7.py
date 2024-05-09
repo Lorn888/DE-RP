c4991acbdeac1f7698325b481be26ceb9e5728b2
Patryk <patrykokpara@hotmail.com>
2024-05-09 20:58:49 +0100

    Fractals

---
+++ PI-Python-practice/Excercises/ex7.py
@@ -0,0 +1,3 @@
+#   a. Write a function to calculate the sum of two numbers. Call the function with different arguments and print the result.
+#   b. Write a function to check if a number is even. Print "Even" or "Odd" based on the result.
+#   c. Write a function that takes a list as input and returns the maximum value in the list.
