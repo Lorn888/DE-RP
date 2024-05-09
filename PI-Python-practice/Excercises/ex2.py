6888bde969a2103680f6ad44bde3a6b33a7edd31
Patryk <patrykokpara@hotmail.com>
2024-05-09 09:10:34 +0100

    ex1

--- PI-Python-practice/Excercises/ex2.py
+++ PI-Python-practice/Excercises/ex2.py
@@ -2,3 +2,7 @@
 # a. Write a for loop to print numbers from 1 to 5.
 # b. Write a while loop to print even numbers from 2 to 6.
 # c. Write an if statement to check if a number is greater than 10. Print a message accordingly.
+
+list = [1,2,3,4,5]
+for item in list:
+    print(item)
