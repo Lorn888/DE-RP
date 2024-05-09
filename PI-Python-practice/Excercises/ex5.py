2f56cd5dc816666d4d145f1ffec071127fa5c0ec
Patryk <patrykokpara@hotmail.com>
2024-05-09 20:27:27 +0100

    Dictionaries

---
+++ PI-Python-practice/Excercises/ex5.py
@@ -0,0 +1,4 @@
+#    a. Create a dictionary representing a person's information (e.g., name, age, city).
+#    b. Print the value associated with the "age" key.
+#    c. Modify the value associated with the "city" key and print the updated dictionary.
+
