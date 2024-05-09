0e0f7b18690394c52c4397169de72ec69fda2d24
Patryk <patrykokpara@hotmail.com>
2024-05-09 14:07:22 +0100

    Lists and Tuples

---
+++ PI-Python-practice/Excercises/ex4.py
@@ -0,0 +1,4 @@
+# a. Create a list with three elements. Append a new element to the list and print it.
+# b. Create a tuple with two elements. Try to modify one of its elements and observe the error.
+# c. Iterate over a list of names and print each name.
+
