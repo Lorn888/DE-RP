9b3b8ac5f864ba694d409df6566dd37e794944ab
Patryk <patrykokpara@hotmail.com>
2024-05-09 21:03:08 +0100

    ex1

--- PI-Python-practice/Excercises/ex7.py
+++ PI-Python-practice/Excercises/ex7.py
@@ -1,3 +1,12 @@
 #    a. Write a recursive function to calculate the factorial of a number.
 #    b. Write a recursive function to calculate the sum of all numbers from 1 to n.
 
+# a)
+
+def fractal(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fractal(n-1)
+
+print(fractal(5))
